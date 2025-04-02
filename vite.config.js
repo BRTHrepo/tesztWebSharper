63e1d5e711fd97bfeb665cfb416ee46c95e76fe4
szabyy <szabyy72@gmail.com>
2025-04-02 11:52:21 +0200

    vite nélkül

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-﻿import { defineConfig } from 'vite';
-
-export default defineConfig({
-    root: './bin/html', // Az index.html helye
-    build: {
-        outDir: '../dist', // Az output könyvtár helye
-        emptyOutDir: true,
-    },
-});
