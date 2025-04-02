48a9452669f97f0558188dd4f5982abdd38638fb
szabyy <szabyy72@gmail.com>
2025-04-02 11:04:47 +0200

    vite js     <PackageReference Include="WebSharper" Version="6.1.9.513" />

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: './bin/html', // Az index.html helye
+    build: {
+        outDir: '../dist', // Az output könyvtár helye
+        emptyOutDir: true,
+    },
+});
