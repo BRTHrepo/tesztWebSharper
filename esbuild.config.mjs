0ffb8fc120c18973299e4bf90bac9ae877078165
szabyy <szabyy72@gmail.com>
2025-04-03 09:19:10 +0200

    ESBuildBundle config

---
+++ esbuild.config.mjs
@@ -0,0 +1,23 @@
+﻿import { readdirSync, cpSync } from 'fs'
+import { build } from 'esbuild'
+
+cpSync('./build/', './bin/html/', { recursive: true, filter: source => source.endsWith('build') || source.endsWith('.html') });
+
+const files = readdirSync('./bin/html/Scripts/WebSharper/JsonToolboxWebApp/');
+
+files.forEach(file => {
+  if (file.endsWith('.js')) {
+    var options =
+    {
+      entryPoints: ['./bin/html/Scripts/WebSharper/JsonToolboxWebApp/' + file],
+      bundle: true,
+      minify: true,
+      format: 'iife',
+      outfile: 'bin/html/Scripts/WebSharper/' + file,
+      globalName: 'wsbundle'
+    };
+
+    console.log("Bundling:", file);
+    build(options);
+  }
+});
